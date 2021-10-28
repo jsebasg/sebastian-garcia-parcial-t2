06eb84913dcd28f9bb3b30b9f4382cb80aeb945a
jua nsebastina garcia hincapie <juan.garcia-h@mail.escuelaing.edu.co>
2021-10-28 07:50:40 -0500

    base Proyect

---
+++ src/main/java/edu/eci/escuelaing/covidParcial/CovidParcialApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.escuelaing.covidParcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidParcialApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/eci/escuelaing/covidParcial/controller/CovidService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.escuelaing.covidParcial.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ *
+ * @author jgarc
+ */
+
+@RestController
+public class CovidService {
+    
+}
